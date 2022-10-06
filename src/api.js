b032788c735eb294dd391f854a5ac01d3c533ade
Oonie11 <utkarshpatel002@gmail.com>
2022-10-06 12:36:49 -0400

    add current weather working

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "48fa7bfc2cmsh80d4085f1bedc41p10ef56jsn9b4c4ddbfde9",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6998bc01d33c7269ddb10c30e43bb556";
