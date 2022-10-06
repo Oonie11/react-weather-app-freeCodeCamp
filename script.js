42be8c407a8c0438aec7d163050ff9f65144429b
Oonie11 <utkarshpatel002@gmail.com>
2022-10-06 09:12:17 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '48fa7bfc2cmsh80d4085f1bedc41p10ef56jsn9b4c4ddbfde9',
+// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
